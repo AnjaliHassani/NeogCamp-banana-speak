23a55bc963a86acc490bb0c9d38464f02f6e59e3
AnjaliHassani <71316589+AnjaliHassani@users.noreply.github.com>
2021-01-04 22:43:20 +0530

    updated app.js
    added translation api

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var ServerUrl = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler(){
-    outputDiv.innerText = "asasasgdcsud  "+ txtInput.value;
-    console.log("Clicked");
+function getTranslationURL(text) {
+    return ServerUrl + "?" + "text=" + text;
+}
+
+function errorhandler(error) {
+    alert("error occured!!", error);
+}
+
+function clickHandler() {
+    // console.log("clicked");
+    // console.log(txtInput.value);
+    var inputText = txtInput.value;
+
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorhandler);
+
 
 };
-btnTranslate.addEventListener("click",clickHandler)
-
-
-
-
-
+btnTranslate.addEventListener("click", clickHandler);
