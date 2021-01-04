103e8858c6c99cc0022655fadecfe755cc5ca201
AnjaliHassani <71316589+AnjaliHassani@users.noreply.github.com>
2021-01-04 12:53:20 +0530

    as
    as

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var outputDiv = document.querySelector("#output");
 
 function clickHandler(){
     outputDiv.innerText = "asasasgdcsud  "+ txtInput.value;
+    console.log("Clicked");
 
 };
 btnTranslate.addEventListener("click",clickHandler)
+
+
 
 
 
